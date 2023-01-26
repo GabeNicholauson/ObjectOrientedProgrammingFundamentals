7a1d0032e916841630f46a146cfad0b7dad4dd94
Gabriel <gnicholauson@gmail.com>
2023-01-25 18:56:19 -0600

    add vending machiner class properties and constructor

--- Classes.cs
+++ Classes.cs
@@ -6,5 +6,51 @@ using System.Threading.Tasks;
 
 namespace ObjectOrientedProgrammingFundamentals_Lab1
 {
-    
+    public class VendingMachine
+    {
+        /**** Properties ****/
+        public int SerialNumber
+        {
+            get;
+        }
+
+        public Dictionary<int, int> MoneyFloat
+        {
+            get; set;
+        }
+
+        public Dictionary<string, int> Inventory
+        {
+            get; set;
+        }
+        /**** Methods ****/
+
+        /**** Constructors ****/
+        public VendingMachine() 
+        {
+            SerialNumber = 15434123;
+            MoneyFloat = new Dictionary<int, int>()
+            {
+                { 1, 15 },
+                { 2, 15 },
+                { 5, 15 },
+                { 10, 15 },
+                { 20, 15 },
+            };
+            Inventory = new Dictionary<string, int>()
+            {
+                { "A", 3 },
+                { "B", 2 },
+                { "C", 1 },
+                { "D", 4}
+            };
+        }
+    }
+
+    public class Product 
+    {
+
+    }
+
+
 }
