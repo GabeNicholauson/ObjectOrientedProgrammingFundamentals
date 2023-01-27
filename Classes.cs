09dc1e67d42118e553c8889815492bc8a7356ac1
Gabriel <gnicholauson@gmail.com>
2023-01-27 15:52:18 -0600

    user can add and remove cars from the car park

--- Classes.cs
+++ Classes.cs
@@ -24,11 +24,12 @@ namespace Car_park
                 }
             }
             throw new Exception("The car park is currently full. Please remove" +
-                "a car and try again");
+                " a car and try again");
         }
 
         public static void RemoveVehicle(string license)
         {
+            bool noCar = true;
             foreach (ParkingSpot spot in ParkingSpots)
             {
                 if (spot.Vehicle != null && spot.Vehicle.LicenseNumber == license)
@@ -36,7 +37,12 @@ namespace Car_park
                     spot.Vehicle.ParkingSpots.Remove(spot);
                     spot.Vehicle = null;
                     Console.WriteLine($"Removed {license} from spot {spot.Number}.");
+                    noCar = false;
                 }
+            }
+            if (noCar)
+            {
+                throw new Exception("That car isn't in the car park. Please try again");
             }
         }
 
@@ -47,7 +53,7 @@ namespace Car_park
 
         static CarPark()
         {
-            Capacity = 300;
+            Capacity = 2;
             for (int i = 1; i <= Capacity; i++)
             {
                 ParkingSpots.Add(new ParkingSpot(i.ToString(), null));
--- Program.cs
+++ Program.cs
@@ -2,7 +2,52 @@
 
 Vehicle vehicle1 = new Vehicle("1212");
 Vehicle vehicle2 = new Vehicle("fbhefb");
+Vehicle vehicle3 = new Vehicle("grgni3o");
+Vehicle vehicle4 = new Vehicle("grbub3");
 
-CarPark.ParkVehicle(vehicle1);
+HashSet<Vehicle> allVehicles = new HashSet<Vehicle>()
+{
+    vehicle1,
+    vehicle2, 
+    vehicle3, 
+    vehicle4
+};
 
-Console.WriteLine(String.Join(", ", vehicle1.ParkingSpots));
+while (true)
+{
+    Console.WriteLine("Would you like to add or remove a car from the car park?");
+    string userInput = Console.ReadLine();
+
+    if (userInput.ToLower() == "add")
+    {
+        Console.WriteLine("Which license would you like to add");
+        userInput = Console.ReadLine();
+
+        foreach(Vehicle vehicle in allVehicles)
+        {
+            if (vehicle.LicenseNumber == userInput)
+            {
+                try
+                {
+                    CarPark.ParkVehicle(vehicle);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    } else if (userInput.ToLower() == "remove")
+    {
+        Console.WriteLine("Which license would you like to remove");
+        userInput = Console.ReadLine();
+
+        try
+        {
+            CarPark.RemoveVehicle(userInput);
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
